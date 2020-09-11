9f2527f1087e0c141142951b0a600c91325bef57
iheb <ihebhamad514@gmail.com>
2020-09-11 09:31:34 +0100

    NO VERBOSE OUTPUT WILL BE SHOWN AT THE INSTALLATION PROCESS

--- install.sh
+++ install.sh
@@ -7,41 +7,50 @@ echo -e "\e[33m [installing the needed requirements] "
 echo ' '
 echo -e "\e[33m [Airgeddon]"
 echo ' '
-git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git
+git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git &> /dev/null
+echo "  Installation Completed"
 echo ' '
 echo -e "\e[33m [WAScan]"
 echo ' '
-git clone https://github.com/m4ll0k/WAScan.git wascan
+git clone https://github.com/m4ll0k/WAScan.git wascan &> /dev/null
 cd wascan
-pip install BeautifulSoup
+pip install BeautifulSoup &> /dev/null
 cd ..
 echo ' '
+echo "  Installation Completed"
+echo ''
 echo -e "\e[33m [Scant3r]"
 echo ''
-git clone https://github.com/knassar702/scant3r.git
+git clone https://github.com/knassar702/scant3r.git &> /dev/null
 cd scant3r
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt &> /dev/null
 cd ..
 echo ''
-echo -e "\e[33m [Lynis]"
-git clone https://github.com/CISOfy/lynis.git
+echo "  Installation Completed"
 echo ''
+echo -e "\e[33m [Lynis]"
+git clone https://github.com/CISOfy/lynis.git &> /dev/null
+echo '  Installation Completed'
+echo ' '
 ## OSINT TOOLS INSTALLATION
 ## OSINT Framework resides here cause I don't get it from github
 echo -e "\e[33m [Photon]"
-git clone https://github.com/s0md3v/Photon
+git clone https://github.com/s0md3v/Photon &> /dev/null
 cd Photon
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt &> /dev/null
 cd ..
+echo '  Installation Completed'
 echo ' '
 echo -e "\e[33m [Sherlock]"
-git clone https://github.com/sherlock-project/sherlock
+git clone https://github.com/sherlock-project/sherlock &> /dev/null
 cd sherlock
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt &> /dev/null
 cd ..
+echo '  Installation Completed'
 echo ' '
 echo -e "\e[33m [Dirsearch]"
-git clone https://github.com/maurosoria/dirsearch
+git clone https://github.com/maurosoria/dirsearch &> /dev/null
+echo '  Installation Completed'
 clear
 chmod +x Hack4Squad.sh
 ./Hack4Squad.sh
