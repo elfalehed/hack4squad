28eafdd4e06669988d3eb1ae53ce57cd752c0ccd
elfalehed <mohamedelfaleh5@gmail.com>
2023-05-31 19:41:46 +0100

    fixing the missed file

--- DIRSEARCH_SIMPLE_V2.py
+++
@@ -1,7 +0,0 @@
-import os, subprocess
-# KMx404 - Hack4Squad
-print()
-url = input("Enter url: ")
-print() 
-subprocess.call("sudo "+"python3 dirsearch/dirsearch.py"+" -u "+url+" "+"-e --min", shell=True)
-
